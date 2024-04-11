a0a0ab37c1f2906d440cfbea3dd24ea0df0adad3
G. Lunardi <g.lunardi@umail.leidenuniv.nl>
2024-04-11 09:30:46 +0200

    Forgot to finished script ha proxy

--- haproxy/build_haproxy.sh
+++ haproxy/build_haproxy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-# 
+# build ha proxy
+podman build -t haproxy .
 
 # Run haproxy-server in podman network from haproxy image
 podman run --replace --name haproxy-server -p 80:80 -p 8084:8084 --network webappnet -d haproxy
