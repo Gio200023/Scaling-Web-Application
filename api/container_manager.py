7ec2be06461f3ce541cd3cac365966e5dcca2af4
Joren van den Berg <jvdberg080403@gmail.com>
2024-04-12 12:24:52 +0200

    dynamically add containers

--- api/container_manager.py
+++ api/container_manager.py
@@ -1,45 +1,96 @@
-import time
+import os
+import shutil
+import sys
 
 from podman import PodmanClient
+from podman.domain.containers import Container
+from podman.errors.exceptions import NotFound
 
 
 class ContainerManager:
+    port = 8081
 
     def __init__(self):
+        print("Initializing haproxy configuration...")
+        shutil.copyfile(src='../haproxy/haproxy.cfg.temp', dst='../haproxy/haproxy.cfg')
+        print("Done")
+        print("Initializing podman client...")
         self.client = PodmanClient(
-            base_url="ssh://core@127.0.0.1:50847/run/user/501/podman/podman.sock",
+            base_url="ssh://root@127.0.0.1:50654/run/podman/podman.sock",
             identity="/Users/jvdberg08/.local/share/containers/podman/machine/machine"
         )
+        print("Done")
+        print("Initializing network...")
+        self.network = self.client.networks.create(name='apinetwork')
+        self.network_ip = self.network.attrs.get('subnets')[0]['gateway']
+        print("Done")
+        print("Creating haproxy container...")
+        self.haproxy_container = self.client.containers.create(
+            image=self.client.images.pull(
+                repository="docker.io/library/haproxy",
+                tag="2.9.7-alpine3.19"
+            ),
+            mounts=[{
+                'type': 'bind',
+                'source': os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])), '../haproxy/haproxy.cfg'),
+                'target': '/usr/local/etc/haproxy/haproxy.cfg',
+                'read_only': True
+            }],
+            ports={'8080': '80', '8081': '8081'},
+            name="haproxy",
+            networks={self.network.name: {self.network.name: self.network.name}},
+        )
+        self.haproxy_container.start()
+        self.haproxy_container.reload()
+        print("Done")
 
     def get_image(self):
-        return self.client.images.get(name="objst:latest")
+        return self.client.images.get(name="localhost/api:latest")
 
     def list(self):
         return self.client.containers.list()
 
     def create(self):
+        self.port += 1
         return self.client.containers.create(
             image=self.get_image(),
-            volumes={'data': {'bind': '/server/data', 'mode': 'rw'}}
+            volumes={'data': {'bind': '/server/data', 'mode': 'rw'}},
+            networks={self.network.name: {self.network.name: self.network.name}},
+            ports={'5001': str(self.port)},
+            name="api" + str(self.port)
         )
+
+    def add_to_haproxy_cfg(self, container: Container, port: str):
+        file = open('../haproxy/haproxy.cfg', 'a')
+        file.write('  server ' + container.name + ' ' + self.network_ip + ':' + port + ' check\n')
+        self.haproxy_container.restart()
 
 
 if __name__ == "__main__":
-    container_manager = ContainerManager()
-    print(container_manager.list())
-    containers = [
-        container_manager.create(),
-        container_manager.create(),
-        container_manager.create(),
-        container_manager.create()
-    ]
-    print(containers)
+    container_manager = None
+    try:
+        container_manager = ContainerManager()
 
-    for container in containers:
+        container = container_manager.create()
         container.start()
         container.reload()
-        print(container.status)
-        print(container.attrs['NetworkSettings']['Networks']['podman']['IPAddress'])
+        container_manager.add_to_haproxy_cfg(container=container, port=str(container_manager.port))
 
-    for container in containers:
-        container.remove(force=True)
+        container = container_manager.create()
+        container.start()
+        container.reload()
+        container_manager.add_to_haproxy_cfg(container=container, port=str(container_manager.port))
+
+        container = container_manager.create()
+        container.start()
+        container.reload()
+        container_manager.add_to_haproxy_cfg(container=container, port=str(container_manager.port))
+
+        while True:
+            pass
+    finally:
+        print("Exiting... (may take a while, 10s for every container)")
+        if container_manager is not None:
+            for container in container_manager.network.containers:
+                container.remove(force=True)
+            container_manager.network.remove(force=True)
--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ podman create \
   --name haproxy \
   -p 80:8080 \
   -p 8081:8081 \
-  -v ./haproxy/haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg \
+  -v ./haproxy/haproxy.cfg.temp:/usr/local/etc/haproxy/haproxy.cfg \
   haproxy:2.9.7-alpine3.19
 podman create -v ./data:/server/data --network apinetwork --name api1 -p 8082:5001 api
 podman create -v ./data:/server/data --network apinetwork --name api2 -p 8083:5001 api
